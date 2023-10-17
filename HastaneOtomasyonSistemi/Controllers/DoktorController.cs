f1b9b0076b3b1f87ed6d415ef2e732413b707d6f
mertAli <66487751+mertalidincer@users.noreply.github.com>
2023-10-17 16:34:31 +0300

    mert
    sadhfasdf

---
+++ HastaneOtomasyonSistemi/Controllers/DoktorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HastaneOtomasyonSistemi.Controllers
+{
+    public class DoktorController : Controller
+    {
+        // GET: Doktor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
