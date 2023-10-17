5d42d09e50fd81d806507efa681358eb39d2568a
Abdullah Haktan <haktanabdullah123@gmail.com>
2023-10-17 16:40:14 +0300

    Sekreter controller oluşturuldu

---
+++ HastaneOtomasyonSistemi/Controllers/SekreterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HastaneOtomasyonSistemi.Controllers
+{
+    public class SekreterController : Controller
+    {
+        // GET: Sekreter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
